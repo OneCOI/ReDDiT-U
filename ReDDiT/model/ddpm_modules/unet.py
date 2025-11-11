b6c82be48c27272ebb92651aa024f86eb42fd994
OneCOI <3599357821@qq.com>
2025-11-11 16:07:12 +0800

    U

--- ReDDiT/model/ddpm_modules/unet.py
+++ ReDDiT/model/ddpm_modules/unet.py
@@ -10,6 +10,175 @@ import numpy as np
 def exists(x):
     return x is not None
 
+
+
+
+class LayerNorm(nn.Module):
+    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first. 
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
+    with shape (batch_size, channels, height, width).
+    """
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_first"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError 
+        self.normalized_shape = (normalized_shape, )
+    
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
+
+class NormDownsample(nn.Module):
+    def __init__(self,in_ch,out_ch,scale=0.5,use_norm=False):
+        super(NormDownsample, self).__init__()
+        self.use_norm=use_norm
+        if self.use_norm:
+            self.norm=LayerNorm(out_ch)
+        self.prelu = nn.PReLU()
+        self.down = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch,kernel_size=3,stride=1, padding=1, bias=False),
+            nn.UpsamplingBilinear2d(scale_factor=scale))
+    def forward(self, x):
+        x = self.down(x)
+        x = self.prelu(x)
+        if self.use_norm:
+            x = self.norm(x)
+            return x
+        else:
+            return x
+
+class NormUpsample(nn.Module):
+    def __init__(self, in_ch,out_ch,scale=2,use_norm=False):
+        super(NormUpsample, self).__init__()
+        self.use_norm=use_norm
+        if self.use_norm:
+            self.norm=LayerNorm(out_ch)
+        self.prelu = nn.PReLU()
+        self.up_scale = nn.Sequential(
+            nn.Conv2d(in_ch,out_ch,kernel_size=3,stride=1, padding=1, bias=False),
+            nn.UpsamplingBilinear2d(scale_factor=scale))
+        self.up = nn.Conv2d(out_ch*2,out_ch,kernel_size=1,stride=1, padding=0, bias=False)
+            
+    def forward(self, x,y):
+        x = self.up_scale(x)
+        x = torch.cat([x, y],dim=1)
+        x = self.up(x)
+        x = self.prelu(x)
+        if self.use_norm:
+            return self.norm(x)
+        else:
+            return x
+
+
+# Cross Attention Block
+class CAB(nn.Module):
+    def __init__(self, dim, num_heads, bias):
+        super(CAB, self).__init__()
+        self.num_heads = num_heads
+        self.temperature = nn.Parameter(torch.ones(num_heads, 1, 1))
+
+        self.q = nn.Conv2d(dim, dim, kernel_size=1, bias=bias)
+        self.q_dwconv = nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=1, groups=dim, bias=bias)
+        self.kv = nn.Conv2d(dim, dim*2, kernel_size=1, bias=bias)
+        self.kv_dwconv = nn.Conv2d(dim*2, dim*2, kernel_size=3, stride=1, padding=1, groups=dim*2, bias=bias)
+        self.project_out = nn.Conv2d(dim, dim, kernel_size=1, bias=bias)
+
+    def forward(self, x, y):
+        b, c, h, w = x.shape
+
+        q = self.q_dwconv(self.q(x))
+        kv = self.kv_dwconv(self.kv(y))
+        k, v = kv.chunk(2, dim=1)
+
+        q = rearrange(q, 'b (head c) h w -> b head c (h w)', head=self.num_heads)
+        k = rearrange(k, 'b (head c) h w -> b head c (h w)', head=self.num_heads)
+        v = rearrange(v, 'b (head c) h w -> b head c (h w)', head=self.num_heads)
+
+        q = torch.nn.functional.normalize(q, dim=-1)
+        k = torch.nn.functional.normalize(k, dim=-1)
+
+        attn = (q @ k.transpose(-2, -1)) * self.temperature
+        attn = nn.functional.softmax(attn,dim=-1)
+
+        out = (attn @ v)
+
+        out = rearrange(out, 'b head c (h w) -> b (head c) h w', head=self.num_heads, h=h, w=w)
+
+        out = self.project_out(out)
+        return out
+    
+
+# Intensity Enhancement Layer
+class IEL(nn.Module):
+    def __init__(self, dim, ffn_expansion_factor=2.66, bias=False):
+        super(IEL, self).__init__()
+
+        hidden_features = int(dim*ffn_expansion_factor)
+
+        self.project_in = nn.Conv2d(dim, hidden_features*2, kernel_size=1, bias=bias)
+        
+        self.dwconv = nn.Conv2d(hidden_features*2, hidden_features*2, kernel_size=3, stride=1, padding=1, groups=hidden_features*2, bias=bias)
+        self.dwconv1 = nn.Conv2d(hidden_features, hidden_features, kernel_size=3, stride=1, padding=1, groups=hidden_features, bias=bias)
+        self.dwconv2 = nn.Conv2d(hidden_features, hidden_features, kernel_size=3, stride=1, padding=1, groups=hidden_features, bias=bias)
+       
+        self.project_out = nn.Conv2d(hidden_features, dim, kernel_size=1, bias=bias)
+
+        self.Tanh = nn.Tanh()
+    def forward(self, x):
+        x = self.project_in(x)
+        x1, x2 = self.dwconv(x).chunk(2, dim=1)
+        x1 = self.Tanh(self.dwconv1(x1)) + x1
+        x2 = self.Tanh(self.dwconv2(x2)) + x2
+        x = x1 * x2
+        x = self.project_out(x)
+        return x
+  
+  
+# Lightweight Cross Attention
+class HV_LCA(nn.Module):
+    def __init__(self, dim,num_heads, bias=False):
+        super(HV_LCA, self).__init__()
+        self.gdfn = IEL(dim) # IEL and CDL have same structure
+        self.norm = LayerNorm(dim)
+        self.ffn = CAB(dim, num_heads, bias)
+        
+    def forward(self, x, y):
+        x = x + self.ffn(self.norm(x),self.norm(y))
+        x = self.gdfn(self.norm(x))
+        return x
+    
+class I_LCA(nn.Module):
+    def __init__(self, dim,num_heads, bias=False):
+        super(I_LCA, self).__init__()
+        self.norm = LayerNorm(dim)
+        self.gdfn = IEL(dim)
+        self.ffn = CAB(dim, num_heads, bias=bias)
+        
+    def forward(self, x, y):
+        x = x + self.ffn(self.norm(x),self.norm(y))
+        x = x + self.gdfn(self.norm(x)) 
+        return x
+
+
+
+
+
+
+
+
+
+
 # PositionalEncoding Source： https://github.com/lmnt-com/wavegrad/blob/master/src/wavegrad/model.py
 class PositionalEncoding(nn.Module):
     def __init__(self, dim):
@@ -89,6 +258,28 @@ class Block(nn.Module):
         return self.block(x)
 
 
+class AttentionBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.channels = channels
+        self.norm = nn.GroupNorm(32, channels)
+        self.q = nn.Conv2d(channels, channels, 1)
+        self.k = nn.Conv2d(channels, channels, 1)
+        self.v = nn.Conv2d(channels, channels, 1)
+        self.proj_out = nn.Conv2d(channels, channels, 1)
+        
+    def forward(self, x):
+        b, c, h, w = x.shape
+        x_norm = self.norm(x)
+        q = self.q(x_norm).view(b, c, -1)
+        k = self.k(x_norm).view(b, c, -1)
+        v = self.v(x_norm).view(b, c, -1)
+        
+        attn = torch.softmax(torch.bmm(q.transpose(1, 2), k) / (c ** 0.5), dim=-1)
+        out = torch.bmm(v, attn.transpose(1, 2)).view(b, c, h, w)
+        return x + self.proj_out(out)
+
+
 class ResnetBlock(nn.Module):
     def __init__(self, dim, dim_out, time_emb_dim=None, dropout=0, norm_groups=32):
         super().__init__()
@@ -109,7 +300,9 @@ class ResnetBlock(nn.Module):
         h = self.noise_func(h, time_emb)
         h = self.block2(h)
         return h + self.res_conv(x)
-
+        
+        
+from einops import rearrange
 
 class SelfAttention(nn.Module):
     def __init__(self, in_channel, n_head=1, norm_groups=32):
@@ -121,11 +314,22 @@ class SelfAttention(nn.Module):
         self.qkv = nn.Conv2d(in_channel, in_channel * 3, 1, bias=False)
         self.out = nn.Conv2d(in_channel, in_channel, 1)
 
+
+        self.dim1=in_channel
+        self.heads=4
+
+        self.i_lca=I_LCA(dim=self.dim1,num_heads=self.heads)
+
     def forward(self, input):
+        # print("input.shape:",input.shape)
+
+        input= self.i_lca(input,input)
+
+
         batch, channel, height, width = input.shape
         n_head = self.n_head
         head_dim = channel // n_head
-
+        
         norm = self.norm(input)
         qkv = self.qkv(norm).view(batch, n_head, head_dim * 3, height, width)
         query, key, value = qkv.chunk(3, dim=2)  # bhdyx
@@ -141,6 +345,11 @@ class SelfAttention(nn.Module):
         out = self.out(out.view(batch, channel, height, width))
 
         return out + input
+
+
+
+
+
 
 
 class ResnetBlocWithAttn(nn.Module):
